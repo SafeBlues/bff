4a1f3544a15ca6743a14b7ceb41fab4e233aad26
Joshua McDonald <mcdojj12@gmail.com>
2021-03-24 22:51:22 +1100

    got participant signup working and validation

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from pydantic import BaseModel
 from dotenv import load_dotenv
 from pydantic.networks import EmailStr
 from pydantic.types import Json
-from fastapi.middleware.cors import CORSMiddleware # for fixing cors issues/allowing differnet origins
+# for fixing cors issues/allowing differnet origins
+from fastapi.middleware.cors import CORSMiddleware
 import sqlalchemy
 from starlette.datastructures import QueryParams
 
@@ -24,10 +25,10 @@ import bcrypt
 
 load_dotenv()
 
-db_hostname=os.environ['HOST']
-db_port=int(os.environ['DB_PORT'])
-db_user=os.environ['USER']
-db_pass=os.environ['PASSWORD']
+db_hostname = os.environ['HOST']
+db_port = int(os.environ['DB_PORT'])
+db_user = os.environ['USER']
+db_pass = os.environ['PASSWORD']
 db_name = os.environ['DB_NAME']
 PORT = int(os.environ['PORT'])
 engine = sqlalchemy.create_engine(
@@ -45,60 +46,75 @@ origins = [
     "http://localhost.tiangolo.com",
     "https://localhost.tiangolo.com",
     "http://localhost",
+    "http://localhost:*",
+    "https://localhost:*",
     "http://localhost:8080",
     "http://localhost:8000",
     "http://localhost:3000",
+    "http://0.0.0.0:3000",
+    "http://127.0.0.1:*",
+    "http://127.0.0.1:3000",
+    "https://127.0.0.1:*",
+    # "frontend:3000",
+    # "http://frontend:3000",
+    # "https://frontend:3000",
 ]
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=origins,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    # allow_methods=["*"],
+    allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
 
 
 @app.get('/v1/admins')
 def get_all_admins_api():
-    query='SELECT * FROM admin_accounts'
+    query = 'SELECT * FROM admin_accounts'
     with engine.connect() as connection:
         result = connection.execute(query)
         res = [row for row in result]
         return(res)
 
+
 @app.get('/v1/admins/{id}')
 def get_admin_by_id(id: int) -> Json:
-    query=f'SELECT * FROM admin_accounts WHERE id=%(id)s'
+    query = f'SELECT * FROM admin_accounts WHERE id=%(id)s'
     with engine.connect() as connection:
-        result = connection.execute(query, {'id': id}) 
+        result = connection.execute(query, {'id': id})
         # res = [row for row in result]
         return(result.fetchall())
+
 
 class SignInPayload(BaseModel):
     email: EmailStr
     password: str
 
+
 @app.post('/v1/signin')
 def signin(payload: SignInPayload):
-    query='SELECT id, password FROM participants WHERE email=%(email)s'
+    query = 'SELECT id, password FROM participants WHERE email=%(email)s'
     with engine.connect() as connection:
-        result = connection.execute(query, {'email':payload.email})
+        result = connection.execute(query, {'email': payload.email})
         user_id, user_password = result.fetchone()
-        if  bcrypt.checkpw(payload.password.encode(), user_password.encode('utf-8')):
+        if bcrypt.checkpw(payload.password.encode(), user_password.encode('utf-8')):
             # happy path
             time = str(datetime.now())
             uuid = uuid4()
             # TODO add a way to clear all old tokens in here, or avoid making a
-            # new one if one already exists - but update the created at time? 
+            # new one if one already exists - but update the created at time?
             create_new_uuid = "INSERT INTO login_tokens (user_id, uuid, time_created) VALUES (%(user_id)s, %(uuid)s, %(time)s)"
-            connection.execute(create_new_uuid, {'user_id': user_id, 'uuid': uuid, 'time': time})
-            
-            response = Response(content = str({'passwords_match': True, 'uuid': str(uuid)}))
-            response.set_cookie("Authorization", uuid, httponly=True, samesite='lax', secure=False)
+            connection.execute(create_new_uuid, {
+                               'user_id': user_id, 'uuid': uuid, 'time': time})
+
+            response = Response(content=str(
+                {'passwords_match': True, 'uuid': str(uuid)}))
+            response.set_cookie("Authorization", uuid,
+                                httponly=True, samesite='lax', secure=False)
             return response
             # return({'passwords_match': True, 'uuid': uuid})
         else:
@@ -118,7 +134,7 @@ def validate_token(req: Request):
     """
     uuid = req.cookies['Authorization']
     with engine.connect() as connection:
-        query =  """
+        query = """
                     SELECT * 
                     FROM participants
                     JOIN (login_tokens) ON (participants.id = login_tokens.user_id)
@@ -134,6 +150,8 @@ def validate_token(req: Request):
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect email or password",
         )
+
+
 def validate_admin_token(req: Request):
     """
     A decorator that checks the UUID token of a user, and throws an error if the
@@ -142,7 +160,7 @@ def validate_admin_token(req: Request):
     print(req.cookies)
     uuid = req.cookies['Authorization']
     with engine.connect() as connection:
-        query =  """
+        query = """
                     SELECT * 
                     FROM participants
                     JOIN (login_tokens) ON (participants.id = login_tokens.user_id)
@@ -150,7 +168,7 @@ def validate_admin_token(req: Request):
                 """
         res = connection.execute(query, {'uuid': uuid})
     user = res.fetchone()
-    if user and user.account_type =='admin':
+    if user and user.account_type == 'admin':
         print(f"ADMIN token validated: {user.email}")
         return(req)
     else:
@@ -163,11 +181,11 @@ def validate_admin_token(req: Request):
 
 # @validate_login(Request)
 @app.get('/v1/logged_in_test')
-def logged_in_test(req: Request = Depends(validate_token)): 
+def logged_in_test(req: Request = Depends(validate_token)):
     # TODO change this into a decorator to wrap other endpoints in.
     uuid = req.cookies['Authorization']
     with engine.connect() as connection:
-        query =  """
+        query = """
                     SELECT * 
                     FROM participants
                     JOIN (login_tokens) ON (participants.id = login_tokens.user_id)
@@ -177,12 +195,14 @@ def logged_in_test(req: Request = Depends(validate_token)):
     user = res.fetchone()
     return(f'logged in as {user.first_name} {user.last_name} email:{user.email}')
 
+
 @app.get('/v1/uuid4')
 def get_uuid():
     """
     Just generates a UUID4 for testing
     """
     return(uuid4())
+
 
 @app.get('/v1/view_cookie')
 def view_cookies(req: Request):
@@ -198,29 +218,62 @@ class Participant(BaseModel):
 
 @app.get('/v1/participants')
 def get_all_participants(req: Request = Depends(validate_admin_token)):
-    query='SELECT * FROM participants'
+    query = 'SELECT * FROM participants'
     with engine.connect() as connection:
         result = connection.execute(query)
         return(result.fetchall())
 
+
 @app.get('/v1/participants/{id}')
 def get_participant_by_id(id: int) -> Json:
-    query=f'SELECT * FROM participants WHERE id=%(id)s'
+    query = f'SELECT * FROM participants WHERE id=%(id)s'
     with engine.connect() as connection:
         result = connection.execute(query, {'id': id})
         return(result.fetchall())
 
+
 @app.post('/v1/participants')
 def create_Participant(participant: Participant):
 
-    encrypted_password = bcrypt.hashpw(participant.password.encode(), bcrypt.gensalt())
-    
-    
+    encrypted_password = bcrypt.hashpw(
+        participant.password.encode(), bcrypt.gensalt())
+
     query = "INSERT INTO participants (first_name, last_name, email, password) " \
             'VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);'
     with engine.connect() as connection:
-        result = connection.execute(query,{"first_name": participant.first_name, "last_name": participant.last_name, "email": participant.email, "password": encrypted_password})
+        result = connection.execute(query, {"first_name": participant.first_name,
+                                            "last_name": participant.last_name, "email": participant.email, "password": encrypted_password})
         return 'success'
+
+
+class Participant2(BaseModel):
+    email: EmailStr
+    participant_id: int
+
+@app.post('/v2/participants')
+def create_Participant2(participant: Participant2):
+    if len(str(participant.participant_id)) != 10:
+        detail = [ # recreating fastAPI typing error for custom error
+            {
+                "loc": [
+                    "body",
+                    "participant_id"
+                ],
+                "msg": "participant_id is the wrong length",
+                "type": "value_error.participant_id"
+            }
+        ]
+        raise HTTPException(status_code=422, detail=detail)
+    query = "INSERT INTO participants (email, participant_id) " \
+            'VALUES (%(email)s, %(participant_id)s);'
+    with engine.connect() as connection:
+        result = connection.execute(
+            query, {"email": participant.email, "participant_id": participant.participant_id})
+        # TODO check for success
+        # TODO set a uuid for the user at the same time
+        return {"status": 200}
+        # TODO validate that the participant id actually exists
+        # TODO return a setcookie with a uuid for sign in
 
 
 if __name__ == '__main__':
