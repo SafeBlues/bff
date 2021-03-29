84e4acb449e84a11289052429f59e45e0c5eadfe
safeblues-machine-user <contact@safeblues.org>
2021-03-29 10:09:48 +0000

    bugfix to allow the participant.safeblues.org to have CORS with BFF

--- app/main.py
+++ app/main.py
@@ -56,6 +56,7 @@ origins = [
     "http://127.0.0.1:3000",
     "https://127.0.0.1:*",
     "http://130.216.216.231:3000",
+    "http://participant.safeblues.org:3000",
     # "frontend:3000",
     # "http://frontend:3000",
     # "https://frontend:3000",
